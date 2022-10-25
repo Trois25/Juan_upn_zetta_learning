d000eb4a60df3b47dc9b5acc63df8a048e4eec11
Trois25 <89327360+Trois25@users.noreply.github.com>
2022-10-25 20:07:51 +0700

    js task day 2

--- function.js
+++ function.js
@@ -1,19 +1,23 @@
-const book = (
-    {book:"bintang", price:85000, author:"TereLiye"}
-);
+const cart = [
+    {title:"bintang", price:85000, author:"TereLiye"},
+    {title:"bulan", price:80000, author:"TereLiye"},
+    {title:"bumi", price:70000, author:"TereLiye"},
+    {title:"matahari", price:75000, author:"TereLiye"}
+];
 
-function purchasing (book,discount,tax,amount) {
+function purchasing (book,discount,tax) {
     let result = 0, many = 0;
-    for(let i=0; i<amount; i++) {
-        if(book && (i < amount)){
+    for(let i=0; i<cart.length; i++) {
+        if(book[i].title && (i < cart.length)){
             many++;
 
-            const amountdiscount = book.price * discount;
-            let discountprice = book.price - amountdiscount;
-            const amounttax = discountprice * tax;
+            const amountdiscount = book[i].price * (discount/100);
+            let discountprice = book[i].price - amountdiscount;
+            const amounttax = discountprice * (tax/100);
             let totalprice = discountprice + amounttax;            
             console.log("-----------------------------------------");
             console.log("book " + many);
+            console.log("Book Title : " + book[i].title)
             console.log("Amount of discount : " + amountdiscount);
             console.log("Price after discount : " + discountprice);
             console.log("Amount of tax : " + amounttax);
@@ -33,4 +37,4 @@ function purchasing (book,discount,tax,amount) {
     return result;
 }
 
-purchasing(book,0.1,0.1,2)
+purchasing(cart,10,10);
